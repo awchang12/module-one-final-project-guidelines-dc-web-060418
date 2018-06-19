b91b465d5db930d2ef7e474b9b69c6e8439be18e
awchang12 <awchang12@gmail.com>
2018-06-19 12:56:47 -0400

    further along

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,44 +2,41 @@ class User < ActiveRecord::Base
     has_many :saved_hikes
     has_many :hikes, through: :saved_hikes
 
-    def save_hike(hike)
+    def save_hike(num)
         SavedHike.find_or_create_by(
             user_id: self.id,
-            hike_id: hike.id
+            hike_id: num
         )
     end
 
-    def delete_hike(hike)
-        SavedHike.where(user_id: self.id, hike_id: hike.id).destroy_all
-        self.hikes.delete(hike_id: hike.id)
+    def delete_hike(num)
+        SavedHike.where(user_id: self.id, hike_id: num).destroy_all
+        self.hikes.delete(hike_id: num)
 
-        return "you successfully delete #{hike.name}!"
+        puts "you successfully deleted the hike!"
     end
 
     def display_my_hikes
-        i = 1
 
         self.hikes.each do |hike|
-            puts "#{i}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
+            puts "HIKE ID: #{hike.id}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
             puts "-------------------------------------------------------------"
             puts "#{hike.summary}"
             puts ""
-            i+=1
+        
         end
     end
 
 
     def search_hikes_by_difficulty(difficulty)
-        hike_arr = Hike.all.where(difficulty: difficulty)
+        hike_arr = Hike.all.where("LOWER(difficulty) = ?", difficulty.downcase)
 
-        i = 1
 
         hike_arr.each do |hike|
-            puts "#{i}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
+            puts "HIKE ID: #{hike.id}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
             puts "-------------------------------------------------------------"
             puts "#{hike.summary}"
             puts ""
-            i += 1
         end
     end
 
@@ -47,14 +44,12 @@ class User < ActiveRecord::Base
 
         hike_arr = Hike.all.where("length > ? AND length < ?",  min_length, max_length)
 
-        i = 1
 
         hike_arr.each do |hike|
-            puts "#{i}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
+            puts "HIKE ID: #{hike.id}. #{hike.name} - #{hike.length} miles - #{hike.difficulty}"
             puts "-------------------------------------------------------------"
             puts "#{hike.summary}"
             puts ""
-            i += 1
         end
     end
 
--- bin/run.rb
+++ bin/run.rb
@@ -23,7 +23,6 @@ def help
     - help: displays this help message.
     - search by length: searches the list of hikes between given paramters.
     - search by difficulty: searches list of hikes based on difficulty.
-    - save hike: saves a given hike to your hikes.
     - my hikes: displays a list of your saved hikes.
     - delete hike: deletes desired hike from your saved hikes.
     - exit: exits this program."
@@ -55,7 +54,82 @@ until user_response == "exit"
         max_distance = gets.chomp
 
         user.search_hikes_by_length(min_distance, max_distance)
+    
+        puts "Would you like to save a hike? yes or no?"
+
+        puts ""
+
+        users_yes_or_no = gets.chomp
+
+        if users_yes_or_no == "yes"
+            puts "Please enter a Hike ID"
+
+            users_hike_id_response = gets.chomp
+
+            user.save_hike(users_hike_id_response)
+
+            puts "Hike has been saved to your hikes"
+     
+        end
+
+        puts ""
+    
+    elsif user_response == "search by difficulty"
+        puts "The difficulty selections are listed below:
+        - green - least strenuous
+        - greenBlue -
+        - blue- 
+        - blueBlack -
+        - black - most strenuous"
+
+        puts "please enter difficulty"
+        
+        user_difficulty = gets.chomp
+
+        user.search_hikes_by_difficulty(user_difficulty)
+
+        puts "Would you like to save a hike? yes or no?"
+
+        puts ""
+
+        users_yes_or_no = gets.chomp
+
+        if users_yes_or_no == "yes"
+            puts "Please enter a Hike ID"
+
+            users_hike_id_response = gets.chomp
+
+            puts "Hike has been saved to your hikes"
+
+            user.save_hike(users_hike_id_response)
+            
+            
+        end
+
+        puts ""
+
+    elsif user_response == "my hikes"
+        puts "Your awesome hikes"
+        user.display_my_hikes
+
+    elsif user_response == "delete hike"
+        user.display_my_hikes
+        
+        puts "Please enter a Hike ID"
+
+        users_hike_id_response = gets.chomp
+
+        user.delete_hike(users_hike_id_response)
+
+        puts ""
+
+    else
+        puts "please enter valid command"
+        puts ""
+
     end
+    help
+
     puts "please enter a command:"
     user_response = gets.chomp
 end
